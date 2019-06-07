69da9d33464658fcc0d27cc196442d1399b8b507
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-06-06 19:47:48 -0500

    Changes center of map, adds a marker and info window at the Golden Gate Bridge

--- src/main/webapp/js/map-loader.js
+++ src/main/webapp/js/map-loader.js
@@ -1,6 +1,18 @@
 function createMap(){
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.422, lng: -122.084},
-    zoom: 16
+    center: {lat: 37.764419, lng: -122.445911},
+    zoom: 12
   });
+
+  const bridgeMarker = new google.maps.Marker({
+    position: {lat: 37.819021, lng: -122.477946},
+    map: map,
+    title: 'Golden Gate Bridge'
+  });
+
+  const bridgeInfoWindow = new google.maps.InfoWindow({
+    content: "This is the Golden Gate Bridge"
+  });
+  bridgeInfoWindow.open(map, bridgeMarker);
+
 }
