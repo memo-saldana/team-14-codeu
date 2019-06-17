7ad5366a2c2ea8a358ddcb53695d266e7a291774
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-06-17 11:20:48 -0500

    Adds clusters of markers, and an infoWindow for each marker

--- src/main/webapp/js/zipCodeLoader.js
+++ src/main/webapp/js/zipCodeLoader.js
@@ -1,18 +1,32 @@
 function createZipCodeMap(){
   fetch('/zipcode-data').then((response) => {
     return response.json();
-  }).then((zipcodes) => {
+  })
+  .then((zipcodes) => {
       
     const map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 35.78613674, lng: -119.4491591},
       zoom:5
     });
 
-    zipcodes.forEach((zipcodeLocation) => {
-      new google.maps.Marker({
+    var zipMarkers = zipcodes.map((zipcodeLocation) => {
+      const marker =  new google.maps.Marker({
         position: {lat: zipcodeLocation.lat, lng: zipcodeLocation.lng},
         map: map
-      });  
+      });
+
+      const info = new google.maps.InfoWindow({
+        content: `${zipcodeLocation.zip} - ${zipcodeLocation.city}, ${zipcodeLocation.state}`
+      });
+
+      marker.addListener('click', () => {
+        info.open(map, marker)
+      });
+      return marker;
     });
+    
+    var markerCluster = new MarkerClusterer(map, zipMarkers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    
   });
 }
