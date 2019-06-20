d2fea3bc65447ae7904ee09e4d24aa249e5c2fa2
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-06-20 15:28:38 -0500

    Adds a new marker on click, with static text

--- src/main/webapp/js/map-loader.js
+++ src/main/webapp/js/map-loader.js
@@ -1,3 +1,5 @@
+let editMarker; 
+let map;
  /**
  * Adds a new marker with a toggable info window
  * @param {Map} map 
@@ -24,15 +26,35 @@ function addLandmark(map, lat, lng, title, description){
  * Main function
  */
 function createMap(){
-  const map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.764419, lng: -122.445911},
     zoom: 12
   });
-
+  map.addListener('click', (event) => {
+    createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+  })
   addLandmark(map, 37.819021, -122.477946, "Golden Gate Bridge", "This is the Golden Gate Bridge")
   addLandmark(map, 37.802122, -122.418805, "Lombard Street", "This is Lombard St.")
   addLandmark(map, 37.811039, -122.415175, "Fishermans Wharf", "This is Fishermans Wharf")
 
-
 }
 
+ /** Creates a marker that shows a textbox the user can edit. */
+ function createMarkerForEdit(lat, lng){
+  // If we're already showing an editable marker, then remove it.
+  if(editMarker){
+   editMarker.setMap(null);
+  }
+  editMarker = new google.maps.Marker({
+    position: {lat: lat, lng: lng},
+    map: map
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: 'Marker for testing'
+  });
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editMarker.setMap(null);
+  });
+  infoWindow.open(map, editMarker);
+ }
